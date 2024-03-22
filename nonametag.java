f4a1a2d85cf15557ff28c190c7b96caa9fa71d83
XenoidXD <90513455+XenoidXD@users.noreply.github.com>
2024-03-22 13:13:37 +0700

    nonametag beta-1.0

---
+++ nonametag.java
@@ -0,0 +1,22 @@
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Nonametag extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        player.setPlayerListName(null);
+        player.setCustomNameVisible(false);
+    }
+}
